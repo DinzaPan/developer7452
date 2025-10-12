13b4037594c79302c6834930164d4ca377afddcd
Dinza <tanjiroplay641@gmail.com>
2025-10-12 13:25:33 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,14 @@
 // Sistema de emojis
 const emojis = {
-    "$risa": "risa.png",
-    "$corazon": "corazon.png",
-    "$estrella": "estrella.png",
-    "$like": "like.png",
-    "$fuego": "fuego.png"
+    "$1": "1.png",
+    "$2": "2.png",
+    "$3": "3.png",
+    "$4": "4.png",
+    "$5": "5.png",
+    "$6": "6.png",
+    "$7": "7.png",
+    "$8": "8.png",
+    "$9": "9.png"
 };
 
 // Función para reemplazar emojis en texto
